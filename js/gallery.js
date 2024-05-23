cdda54418e7fbce8ba56a9c82e56c8d9ded62521
jadavis778 <47931210+jadavis778@users.noreply.github.com>
2024-05-22 23:10:20 -0400

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -31,7 +31,7 @@ function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  // Add the "show-content" class (display:block) to the filtered elements, and remove the "show-content" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show-content");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show-content");
